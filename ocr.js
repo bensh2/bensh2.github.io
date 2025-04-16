c476f7245ad37ba9f25c9ba36b5d99b528c70636
Ben Shellef <benshellef@gmail.com>
2025-04-16 14:56:03 +0300

    fixed get file

--- ocr.js
+++ ocr.js
@@ -1,5 +1,30 @@
 // basic ocr
 
+document.addEventListener('DOMContentLoaded', ready);
+
+function ready()
+{
+    const fileSelect = document.getElementById("fileSelect");
+    const fileElem = document.getElementById("fileElem");
+
+    fileSelect.addEventListener("click",  (e) => {
+        if (fileElem) {
+            fileElem.click();
+        }
+    }, false);
+
+    fileElem.addEventListener("change", readfile, false);
+}
+
+function readfile()
+{
+    const file = document.getElementById("fileElem").files[0];
+    if (!file) { return false };
+    console.log(file);
+    processocr(file);
+    document.getElementById("fileElem").value = null;
+}
+
 async function processocr(file)
 {
     document.getElementById("wait").style.display = "block";
