b8c7a24f1de47e993e8f8e1a32af4479c5d0a7bd
Ben Shellef <benshellef@gmail.com>
2025-05-04 14:55:23 +0300

    attempt form instead of fetch

--- login/googleredir.js
+++ login/googleredir.js
@@ -11,7 +11,7 @@ async function reqUserData()
     const baseurl = "https://www.googleapis.com/auth/userinfo.email";
     const access_token = params.get("access_token");
     let url = baseurl + "?access_token="+access_token;
-    try {
+    /*try {
         const response = await fetch(url);
         if (!response.ok) {
           throw new Error(`Response status: ${response.status}`);
@@ -21,5 +21,24 @@ async function reqUserData()
         console.log(json);
       } catch (error) {
         console.error(error.message);
-    }
+    }*/
+        let form = document.createElement('form');
+        form.setAttribute('method', 'GET'); // Send as a GET request.
+        form.setAttribute('action', baseurl);
+      
+        // Parameters to pass to OAuth 2.0 endpoint.
+        let fparams = { 'access_token': access_token};
+      
+        // Add form parameters as hidden input values.
+        for (var p in fparams) {
+          var input = document.createElement('input');
+          input.setAttribute('type', 'hidden');
+          input.setAttribute('name', p);
+          input.setAttribute('value', fparams[p]);
+          form.appendChild(input);
+        }
+      
+        // Add form to page and submit it to open the OAuth 2.0 endpoint.
+        document.body.appendChild(form);
+        form.submit();
 }
