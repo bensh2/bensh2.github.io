fb5e6af5415b4f3f4d5a9a2335b7ad7e8bad3e3c
Ben Shellef <benshellef@gmail.com>
2025-05-25 12:33:29 +0300

    supabase client

--- apptest/login.js
+++ apptest/login.js
@@ -1,3 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient('https://ujqbqwpjlbmlthwgqdgm.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqcWJxd3BqbGJtbHRod2dxZGdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMzM5MTYsImV4cCI6MjA2MjgwOTkxNn0.8FBSPslplJIIPMYE-f4Ij_nYiB6ut0ElxGxVaoqZLbs');
+
 async function handleSignInWithGoogle(response) 
 {  
     const { data, error } = await supabase.auth.signInWithIdToken({
