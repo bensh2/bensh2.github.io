8ba56c827d061f2927f693d1911c033248cadaea
Ben Shellef <benshellef@gmail.com>
2025-05-25 13:04:22 +0300

    tryin esm

--- apptest/login.js
+++ apptest/login.js
@@ -1,3 +1,22 @@
+
+import { createClient } from 'https://esm.sh/@supabase/supabase-js';  
+const supabase = createClient('https://ujqbqwpjlbmlthwgqdgm.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqcWJxd3BqbGJtbHRod2dxZGdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMzM5MTYsImV4cCI6MjA2MjgwOTkxNn0.8FBSPslplJIIPMYE-f4Ij_nYiB6ut0ElxGxVaoqZLbs');
+
+export async function handleSignInWithGoogle(response) 
+{  
+    //debugger;
+    const { data, error } = await supabase.auth.signInWithIdToken({
+            provider: 'google',
+            token: response.credential,  
+        });
+
+    console.log("Sign in with Google response:", data, error);
+}
+
+window.handleSignInWithGoogle = handleSignInWithGoogle;  // Expose the function to the global scope
+
+/*
+// non-esm version
 const { createClient } = supabase;
 
 const supabaseClient = createClient('https://ujqbqwpjlbmlthwgqdgm.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqcWJxd3BqbGJtbHRod2dxZGdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMzM5MTYsImV4cCI6MjA2MjgwOTkxNn0.8FBSPslplJIIPMYE-f4Ij_nYiB6ut0ElxGxVaoqZLbs');
@@ -12,3 +31,4 @@ async function handleSignInWithGoogle(response)
 
     console.log("Sign in with Google response:", data, error);
 }
+*/
