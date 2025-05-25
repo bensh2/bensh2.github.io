cf7ce6ea8309533f58fff92bd8d30959aac7699e
Ben Shellef <benshellef@gmail.com>
2025-05-25 13:42:18 +0300

    another attempt with esm

--- apptest/login.js
+++ apptest/login.js
@@ -1,21 +1,5 @@
-const { createClient } = supabase;
 
-const supabaseClient = createClient('https://ujqbqwpjlbmlthwgqdgm.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqcWJxd3BqbGJtbHRod2dxZGdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMzM5MTYsImV4cCI6MjA2MjgwOTkxNn0.8FBSPslplJIIPMYE-f4Ij_nYiB6ut0ElxGxVaoqZLbs');
-
-async function handleSignInWithGoogle(response) 
-{  
-    //debugger;
-    const { data, error } = await supabaseClient.auth.signInWithIdToken({
-            provider: 'google',
-            token: response.credential,  
-        });
-
-    console.log("Sign in with Google response:", data, error);
-}
-
-
-/* 
-// esm version, doesn't work because google callback needs to be in the global scope
+// esm version
 import { createClient } from 'https://esm.sh/@supabase/supabase-js';  
 const supabase = createClient('https://ujqbqwpjlbmlthwgqdgm.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqcWJxd3BqbGJtbHRod2dxZGdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMzM5MTYsImV4cCI6MjA2MjgwOTkxNn0.8FBSPslplJIIPMYE-f4Ij_nYiB6ut0ElxGxVaoqZLbs');
 
@@ -30,5 +14,22 @@ export async function handleSignInWithGoogle(response)
     console.log("Sign in with Google response:", data, error);
 }
 
-window.handleSignInWithGoogle = handleSignInWithGoogle;  // Expose the function to the global scope
+//window.handleSignInWithGoogle = handleSignInWithGoogle;  // Expose the function to the global scope
+
+/*
+// non-esm version
+const { createClient } = supabase;
+
+const supabaseClient = createClient('https://ujqbqwpjlbmlthwgqdgm.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqcWJxd3BqbGJtbHRod2dxZGdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMzM5MTYsImV4cCI6MjA2MjgwOTkxNn0.8FBSPslplJIIPMYE-f4Ij_nYiB6ut0ElxGxVaoqZLbs');
+
+async function handleSignInWithGoogle(response) 
+{  
+    //debugger;
+    const { data, error } = await supabaseClient.auth.signInWithIdToken({
+            provider: 'google',
+            token: response.credential,  
+        });
+
+    console.log("Sign in with Google response:", data, error);
+}
 */
