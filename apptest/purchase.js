5fca16f268db7459ec727e75f851b67e0fde9f01
Ben Shellef <benshellef@gmail.com>
2025-05-26 14:53:54 +0300

    trying with supabase.auth

--- apptest/purchase.js
+++ apptest/purchase.js
@@ -1,7 +1,7 @@
-const { createClient, auth } = supabase;
+const { createClient } = supabase;
 
 const supabaseClient = createClient('https://ujqbqwpjlbmlthwgqdgm.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqcWJxd3BqbGJtbHRod2dxZGdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMzM5MTYsImV4cCI6MjA2MjgwOTkxNn0.8FBSPslplJIIPMYE-f4Ij_nYiB6ut0ElxGxVaoqZLbs');
-let userauth = auth.session().access_token;
+let userauth = supabase.auth.session().access_token;
 console.log("User auth token:", userauth);
 
 export async function makePurchaseRequest(priceId, quantity) 
