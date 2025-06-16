90481d5e8005c5a5e07a599d26bac996300050bd
Ben Shellef <benshellef@gmail.com>
2025-06-16 17:42:32 +0300

    added subscription product

--- apptest/product.js
+++ apptest/product.js
@@ -5,7 +5,8 @@ $( function() {
 
     $("#offerButton").click(function() {
         window.location.href = "checkout.html?priceId=price_1RRWvbQon05XhpcyEEuHqMWA"; 
-
     });
-
+    $("#offer2Button").click(function() {
+        window.location.href = "checkout.html?priceId=price_1RadzGQon05XhpcyLacP78Lg"; 
+    });
 });
