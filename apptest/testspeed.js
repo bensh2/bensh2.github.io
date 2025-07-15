c6d58dde95bd7cc9795bdedfe0244f37b2f6ed38
Ben Shellef <benshellef@gmail.com>
2025-07-15 17:40:41 +0300

    feat: add Test Speed page with initial HTML structure and JavaScript functionality

---
+++ apptest/testspeed.js
@@ -0,0 +1,17 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js';  
+import { FunctionsHttpError, FunctionsRelayError, FunctionsFetchError } from "https://esm.sh/@supabase/supabase-js";
+
+const supabase = createClient('https://ujqbqwpjlbmlthwgqdgm.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqcWJxd3BqbGJtbHRod2dxZGdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMzM5MTYsImV4cCI6MjA2MjgwOTkxNn0.8FBSPslplJIIPMYE-f4Ij_nYiB6ut0ElxGxVaoqZLbs');
+
+run();
+
+async function run()
+{
+    document.querySelector("#messages").innerHTML = "Starting speed test...";
+
+    const { data, error } = await supabase.functions.invoke('testspeed', {  body: 
+            JSON.stringify({})
+    });
+
+    document.querySelector("#messages").innerHTML = "Speed test completed.";
+}
